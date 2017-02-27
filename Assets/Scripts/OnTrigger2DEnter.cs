6b257f523a28901354334c09deb9c5f1378131ae
nikecaballo <nikevccaballo@live.com>
2017-02-27 15:01:28 -0800

    OnTriggerEnter2D
    -able to destroy units for both players and enemies when it collides
    with the king
    -decrements king's health based on damage from unit

---
+++ Assets/Scripts/OnTrigger2DEnter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrigger2DEnter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Player unit collides with the king and gets destroyed
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log (col.gameObject.name);
+
+		if (col.gameObject.name.Contains("player")) {
+			EndTurn.EnemyUnits.Remove (col.gameObject);
+			EndTurn.PlayerUnits.Remove (col.gameObject);
+			Destroy (col.gameObject);
+			GetComponent<Unit>().health -= col.gameObject.GetComponent<Unit>().dmg;
+		}
+
+	}
+
+}
