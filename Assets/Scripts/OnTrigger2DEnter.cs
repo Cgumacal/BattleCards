d3ee40a1e322b85e976f0d338739b9832f912986
Cgumacal <chris.gumacal94@gmail.com>
2017-03-15 14:25:30 -0700

    ontrigger2d changes

--- Assets/Scripts/OnTrigger2DEnter.cs
+++ Assets/Scripts/OnTrigger2DEnter.cs
@@ -2,29 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnTrigger2DEnter : MonoBehaviour {
+public class OnTrigger2DEnter : MonoBehaviour
+{
+    public GameObject winnerPanel;
+    public GameObject loserPanel;
+    public GameObject drawPanel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	//Player unit collides with the king and gets destroyed
-	void OnTriggerEnter2D(Collider2D col){
-		Debug.Log (col.gameObject.name);
+    }
 
-		if (col.gameObject.name.Contains("player")) {
-			GameLists.EnemyUnits.Remove (col.gameObject);
-			GameLists.PlayerUnits.Remove (col.gameObject);
-			Destroy (col.gameObject);
-			GetComponent<Unit>().health -= col.gameObject.GetComponent<Unit>().dmg;
-		}
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+    }
+
+    //Player unit collides with the king and gets destroyed
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name);
+
+        if (col.gameObject.name.Contains("player"))
+        {
+
+            GameLists.EnemyUnits.Remove(col.gameObject);
+            GameLists.PlayerUnits.Remove(col.gameObject);
+            Destroy(col.gameObject);
+
+            GetComponent<Unit>().health -= col.gameObject.GetComponent<Unit>().dmg;
+
+            if ((col.gameObject.name.Contains("leftplayer") && GetComponent<Unit>().health <= 0)
+                && (col.gameObject.name.Contains("rightplayer") && GetComponent<Unit>().health <= 0))
+            {
+                drawPanel.SetActive(true);
+            }
+            else if (col.gameObject.name.Contains("leftplayer") && GetComponent<Unit>().health <= 0)
+            {
+                loserPanel.SetActive(true);
+            }
+            else if (col.gameObject.name.Contains("rightplayer") && GetComponent<Unit>().health <= 0)
+            {
+                winnerPanel.SetActive(true);
+            }
+
+            Destroy(col.gameObject);
+        }
+
+    }
 
 }
