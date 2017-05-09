961ce8b1d4114affc0616ca731ded60e34f95e6e
Cgumacal <chris.gumacal94@gmail.com>
2017-05-08 19:34:36 -0700

    Fixed merge error
    fixed the merge error in the card script

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -13,11 +13,11 @@ public class Card : MonoBehaviour
     public int health;
     public int attack;
     public int speed;
-
+    private GameObject player;
 
     void Start()
     {
-          
+        player = EndTurn.ownedPlayer;
     }
 
     void Update()
@@ -35,33 +35,34 @@ public class Card : MonoBehaviour
     public void selectCard()
     {
         Debug.Log("mouse as button up working");
-        GameLists.selectedCard = this.gameObject;
+        GameLists.selectedCard = this.gameObject; //makes sure that the selected card is being referenced
 
-        if (manaCost <= EndTurn.ownedPlayer.GetComponent<Player>().mana)
+        if (manaCost <= EndTurn.ownedPlayer.GetComponent<Player>().mana) //checks to see if the player has sufficient mana to play the selected card
         {
-            if (playerID == 1)
+            if (playerID == 1) //if it's player 1, the left side, or "player side" of the board enables zones to be selected to summon the unit from the selected card
             {
-                foreach (GameObject zone in GameLists.PlayerSummon)
+                foreach (GameObject zone in GameLists.PlayerSummon) //loops through each of the "player" zones
                 {
                     Debug.Log(zone.name + "trying to see if true");
-                    if (!zone.GetComponent<Summon>().canSummon) zone.GetComponent<Summon>().canSummon = true;
+                    if (!zone.GetComponent<Summon>().canSummon) zone.GetComponent<Summon>().canSummon = true; //sets the "player" summon zones to allow a unit to be summoned
                 }
             }
-            else
+            else //if player id is not 1, the right side, or the "enemy side" of the board enables zones to be selected to summon the unit from the selected card
             {
-                foreach (GameObject zone in GameLists.EnemySummon)
+                foreach (GameObject zone in GameLists.EnemySummon) //loops through each of the "enemy" summon zones
                 {
                     Debug.Log(zone.name + "trying to see if true");
-                    if (!zone.GetComponent<Summon>().canSummon) zone.GetComponent<Summon>().canSummon = true;
+                    if (!zone.GetComponent<Summon>().canSummon) zone.GetComponent<Summon>().canSummon = true; //ests the "enemy" summon ones to allow a unit to be summoned
                 }
             }
-        }else
+        }
+        else
         {
-            Debug.Log("Not enough mana");
+            Debug.Log("Not enough mana");//error logged if player does not have sufficient mana to play the card
         }
     }
 
-    public void deselectCard()
+    public void deselectCard() // was supposed to reset all card status so that the player can deselect a card if they desired...this function is still being debugged
     {
         GameLists.selectedCard = this.gameObject;
 
@@ -83,12 +84,13 @@ public class Card : MonoBehaviour
         }
     }
 
-	public void selectTrapCard(){
-		Debug.Log("mouse as button up working");
-		foreach (GameObject zone in GameLists.PlayerTrapSummon)
-		{
-			Debug.Log(zone.name + "trying to see if true");
-			if (!zone.GetComponent<Trap>().canSummon) zone.GetComponent<Trap>().canSummon = true;
-		}
-	}
+    public void selectTrapCard()
+    {
+        Debug.Log("mouse as button up working");
+        foreach (GameObject zone in GameLists.PlayerTrapSummon)
+        {
+            Debug.Log(zone.name + "trying to see if true");
+            if (!zone.GetComponent<Trap>().canSummon) zone.GetComponent<Trap>().canSummon = true;
+        }
+    }
 }
